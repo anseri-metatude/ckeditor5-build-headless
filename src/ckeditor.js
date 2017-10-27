975021146ed894c2d8b34a81e569a02db6279a5d
David Polberger <david@polberger.se>
2017-10-28 01:31:39 +0200

    Added a build with a custom headless editor
    A build containing a custom headless editor and a small number of
    plug-ins is hereby committed.

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -3,66 +3,32 @@
  * For licensing, see LICENSE.md.
  */
 
-import InlineEditorBase from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
+import HeadlessEditorBase from '@davidpolberger/ckeditor5-editor-headless/src/headlesseditor';
 import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadadapterPlugin from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import AutoformatPlugin from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockquotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import EasyimagePlugin from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading';
 import ImagePlugin from '@ckeditor/ckeditor5-image/src/image';
-import ImagecaptionPlugin from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImagestylePlugin from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImagetoolbarPlugin from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
+import LinkenginePlugin from '@ckeditor/ckeditor5-link/src/linkengine';
 import ListPlugin from '@ckeditor/ckeditor5-list/src/list';
 import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import ImageuploadPlugin from '@ckeditor/ckeditor5-upload/src/imageupload';
 
-export default class InlineEditor extends InlineEditorBase {}
+export default class HeadlessEditor extends HeadlessEditorBase {}
 
-InlineEditor.build = {
+HeadlessEditor.build = {
 	plugins: [
 		EssentialsPlugin,
-		UploadadapterPlugin,
 		AutoformatPlugin,
 		BoldPlugin,
 		ItalicPlugin,
 		BlockquotePlugin,
-		EasyimagePlugin,
 		HeadingPlugin,
 		ImagePlugin,
-		ImagecaptionPlugin,
-		ImagestylePlugin,
-		ImagetoolbarPlugin,
-		LinkPlugin,
+		LinkenginePlugin,
 		ListPlugin,
-		ParagraphPlugin,
-		ImageuploadPlugin
+		ParagraphPlugin
 	],
-	config: {
-		toolbar: {
-			items: [
-				'headings',
-				'bold',
-				'italic',
-				'link',
-				'bulletedList',
-				'numberedList',
-				'blockQuote',
-				'undo',
-				'redo'
-			]
-		},
-		image: {
-			toolbar: [
-				'imageStyleFull',
-				'imageStyleSide',
-				'|',
-				'imageTextAlternative'
-			]
-		}
-	}
+	config: {}
 };
